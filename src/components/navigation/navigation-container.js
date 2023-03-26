5374cc5c7c1b42b854c6692bd8c33d5dba567c56
Greg Kimma <gmkimma@gmail.com>
2023-03-26 03:43:16 -0700

    Added files

---
+++ src/components/navigation/navigation-container.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import axios from 'axios'
+import { withRouter } from 'react-router'
+import { NavLink } from 'react-router-dom'
+
+const NavigationComponent = props => {
+  const dynamicLink = (route, linkText) => {
+    return (
+      <div className='nav-link-wrapper'>
+        <NavLink to='/blog'>Blog</NavLink>
+      </div>
+    )
+  }
+
+  const handleSignOut = () => {
+    axios
+      .delete('https://api.devcamp.space/logout', { withCredentials: true })
+      .then(r => {
+        if (r.status === 200) {
+          props.history.push('/')
+          props.handleSuccessfulLogout()
+        }
+        return r.data
+      })
+      .catch(error => {
+        console.log('Error signing out', error)
+      })
+  }
+
+  return (
+    <div className='nav-wrapper'>
+      <div className='left-side'>
+        <div className='nav-link-wrapper'>
+          <NavLink exact to='/'>
+            Home
+          </NavLink>
+        </div>
+        <div className='nav-link-wrapper'>
+          <NavLink to='/about-me'>About</NavLink>
+        </div>
+        <div className='nav-link-wrapper'>
+          <NavLink to='/contact'>Contact</NavLink>
+        </div>
+
+        {props.loggedInStatus === 'LOGGED_IN'
+          ? dynamicLink('/blog', 'Blog')
+          : null}
+
+        <div className='nav-link-wrapper'>
+          {false ? <button>Add Blog</button> : null}
+        </div>
+      </div>
+
+      <div className='right-side'>
+        GREG KIMMA
+        {props.loggedInStatus === 'LOGGED_IN' ? (
+          <a onClick={handleSignOut}>Sign Out</a>
+        ) : null}
+      </div>
+    </div>
+  )
+}
+
+export default withRouter(NavigationComponent)
